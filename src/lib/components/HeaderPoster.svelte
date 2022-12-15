55832d7b974671d43e085d1c4e02256396e39cd0
thel3onardo <leonardo.contato03@gmail.com>
2022-12-15 15:50:14 -0300

    feat: add hover effects to HeaderPoster component

--- src/lib/components/HeaderPoster.svelte
+++ src/lib/components/HeaderPoster.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
-    export let title, description, bgUrl;
+    export let title:string, description:
+    string, bgUrl:string;
 </script>
 
-<div class="w-full flex flex-col justify-end px-11 py-10 rounded-lg min-h-[400px] poster-bg relative z-10" style="background-image: url('{bgUrl}')">
+<div class="w-full flex flex-col justify-end px-11 py-10 rounded-lg min-h-[400px] poster-bg relative z-10 overflow-hidden transition cursor-pointer hover:-translate-y-1.5" style="background-image: url('{bgUrl}')">
     <div class="w-full">
         <h2 class="text-white font-rubik text-4xl font-medium uppercase mb-2">{ title }</h2>
         <p class="font-inter text-gray-200 w-2/4">{ description }</p>
@@ -19,5 +20,9 @@
         width: 100%;
         height: 100%;
         background: linear-gradient(270deg, rgba(6, 9, 15, 0.32) 0%, rgba(6, 9, 15, 0.9) 100%);
+    }
+
+    .poster-bg:hover:before {
+        background: linear-gradient(270deg, rgba(0, 17, 28, 0.5) 0%, rgba(0, 17, 28, 0.9) 100%);
     }
 </style>
