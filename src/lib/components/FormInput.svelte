448e06a9b08966c588528ee64618727b28eae94c
thel3onardo <leonardo.contato03@gmail.com>
2022-12-19 16:07:12 -0300

    feat: Build basic visual structure of FormInput component

---
+++ src/lib/components/FormInput.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let placeholder: string,
+        state: boolean,
+        label: string,
+        block: boolean
+</script>
+
+<form class="flex flex-col my-4" class:w-full="{block}">
+    <label for="input" class="text-white font-semibold mb-1.5">{ label }</label>
+    <input
+        name="input"
+        placeholder="{placeholder}"
+        class="border border-gray-200 rounded-lg text-white bg-transparent px-3 py-2 outline-none focus:border-blue-500 focus:shadow-lg focus:shadow-blue-500/10 transition"
+    />
+</form>
