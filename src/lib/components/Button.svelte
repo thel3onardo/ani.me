357438bd169270632f810f444b883ecefbe956ca
thel3onardo <leonardo.contato03@gmail.com>
2022-12-19 16:42:04 -0300

    feat: finish basic visual structure of login page

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let background: boolean, block: boolean
+</script>
+
+<button
+    class="px-12 py-2 text-white font-bold rounded-lg hover:shadow-lg hover:shadow-blue-500/20 transition"
+    class:w-full="{block}"
+    class:bg-blue-500="{background}"
+>
+    <slot />
+</button>
--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,11 +1,37 @@
 <script lang="ts">
     import image from '$lib/assets/login-image.png'
+    import FormInput from '$lib/components/FormInput.svelte'
+    import ButtonComponent from '$lib/components/Button.svelte'
 </script>
 
 <div class="relative">
-    <div class="flex flex-col bg-dark-900 p-8 w-96 rounded-lg">
+    <div
+        class="flex flex-col items-center bg-dark-900 p-8 w-[400px] rounded-lg"
+    >
         <h1 class="font-bold font-rubik text-white text-4xl">Faça login</h1>
-        <p>oi</p>
+        <FormInput
+            label="E-mail"
+            placeholder="Insira seu e-mail"
+            invalidText="E-mail inválido. Tente novamente"
+            validate="{(e) =>
+        e?.length > 2}"
+            autocomplete="{false}"
+            block
+        />
+
+        <FormInput
+            label="Senha"
+            placeholder="Insira sua senha"
+            type="password"
+            invalidText="Senha inválida. Tente novamente"
+            validate="{(e) =>
+        e?.length > 2}"
+            autocomplete="{false}"
+            block
+        />
+
+        <div class="w-full mt-4">
+            <ButtonComponent background block>Logar</ButtonComponent>
+        </div>
     </div>
-    <!-- <img src="{image}" class="absolute -left-24 -top-24 w-72" /> -->
 </div>
